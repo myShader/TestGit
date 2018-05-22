9a9eeb7126c97efd96d63b9a83223a72467271e7
my <897938585@qq.com>
2018-05-22 17:42:38 +0800

    AR工程

---
+++ ARDemo/Assets/Vuforia/Scripts/DefaultTrackableEventHandler.cs
@@ -0,0 +1,124 @@
+/*==============================================================================
+Copyright (c) 2010-2014 Qualcomm Connected Experiences, Inc.
+All Rights Reserved.
+Confidential and Proprietary - Protected under copyright and other laws.
+==============================================================================*/
+
+using UnityEngine;
+
+namespace Vuforia
+{
+    /// <summary>
+    /// A custom handler that implements the ITrackableEventHandler interface.
+    /// </summary>
+    public class DefaultTrackableEventHandler : MonoBehaviour,
+                                                ITrackableEventHandler
+    {
+        #region PRIVATE_MEMBER_VARIABLES
+ 
+        private TrackableBehaviour mTrackableBehaviour;
+    
+        #endregion // PRIVATE_MEMBER_VARIABLES
+
+
+
+        #region UNTIY_MONOBEHAVIOUR_METHODS
+    
+        void Start()
+        {
+            mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+            if (mTrackableBehaviour)
+            {
+                mTrackableBehaviour.RegisterTrackableEventHandler(this);
+            }
+        }
+
+        #endregion // UNTIY_MONOBEHAVIOUR_METHODS
+
+
+
+        #region PUBLIC_METHODS
+
+        /// <summary>
+        /// Implementation of the ITrackableEventHandler function called when the
+        /// tracking state changes.
+        /// </summary>
+        public void OnTrackableStateChanged(
+                                        TrackableBehaviour.Status previousStatus,
+                                        TrackableBehaviour.Status newStatus)
+        {
+            if (newStatus == TrackableBehaviour.Status.DETECTED ||
+                newStatus == TrackableBehaviour.Status.TRACKED ||
+                newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+            {
+                OnTrackingFound();
+            }
+            else
+            {
+                OnTrackingLost();
+            }
+        }
+
+        #endregion // PUBLIC_METHODS
+
+
+
+        #region PRIVATE_METHODS
+
+
+        private void OnTrackingFound()
+        {
+            Renderer[] rendererComponents = GetComponentsInChildren<Renderer>(true);
+            Collider[] colliderComponents = GetComponentsInChildren<Collider>(true);
+
+            // Enable rendering:
+            foreach (Renderer component in rendererComponents)
+            {
+                component.enabled = true;
+            }
+
+            // Enable colliders:
+            foreach (Collider component in colliderComponents)
+            {
+                component.enabled = true;
+            }
+            CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+            ani = GetComponentInChildren<Animation>();
+            Debug.Log("获取到了动画组件");
+            Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " found");
+        }
+        Animation ani;
+        void OnGUI()
+        {
+            if (GUI.Button(new Rect(100, 100, 300, 100), "Begin"))
+            {
+                ani.Play();
+            }
+            if (GUI.Button(new Rect(500, 100, 300, 100), "Stop"))
+            {
+                ani.Stop();
+            }
+        }
+        private void OnTrackingLost()
+        {
+            Renderer[] rendererComponents = GetComponentsInChildren<Renderer>(true);
+            Collider[] colliderComponents = GetComponentsInChildren<Collider>(true);
+
+            // Disable rendering:
+            foreach (Renderer component in rendererComponents)
+            {
+                component.enabled = false;
+            }
+
+            // Disable colliders:
+            foreach (Collider component in colliderComponents)
+            {
+                component.enabled = false;
+            }
+
+            Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " lost");
+        }
+
+        #endregion // PRIVATE_METHODS
+    }
+}
